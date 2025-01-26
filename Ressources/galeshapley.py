0f5f212e9f8ef30160401851a763fd929cfd84c7
Thibaut M <thibaut.marcq@gmail.com>
2025-01-26 02:24:31 +0100

    Création de Gale-Shapley (algo), modif de lecture spe pour renvoyer un array de int pour cap,  refacto/lisibilité du code

--- Ressources/galeshapley.py
+++ Ressources/galeshapley.py
@@ -1,20 +1,70 @@
 import numpy as np
 
-def galeShapley(tabEtu, tabSpe, cap):
-    capSpe=cap.copy()
-    etuLibre = np.arange(len(tabEtu))
-    speLibre = np.arange(len(tabSpe))
-    tabProp = np.zeros(len(tabEtu), dtype=int)
 
-    while (len(etuLibre) > 0): #s'il y a du monde, c'est qu'ils nont pas proposé aux spes
-        spe = tabEtu[libre[0]][tabProp[libre[0]]]
-        if (capSpe[spe] > 0):
-            capSpe[spe] -= 1
-        #     speLibre = np.delete(speLibre, np.where(speLibre == spe))
-        #     etuLibre = np.delete(etuLibre, 0)
-        # else:
-        #     for i in range(len(tabSpe[spe])):
-        #         if (tabSpe[spe][i] == etuLibre[0]):
-        #             speLibre = np.append(speLibre, spe)
-        #             etuLibre = np.delete(etuLibre, 0)
-        #             break
+# ANCIEN ALGO
+# def galeShapley(tabEtu, tabSpe, cap):
+#     capSpe=cap.copy()
+#     etuLibre = np.arange(len(tabEtu))
+#     speLibre = np.arange(len(tabSpe))
+#     tabProp = np.zeros(len(tabEtu), dtype=int)
+
+#     while (len(etuLibre) > 0): #s'il y a du monde, c'est qu'ils nont pas proposé aux spes
+#         spe = tabEtu[libre[0]][tabProp[libre[0]]]
+#         if (capSpe[spe] > 0):
+#             capSpe[spe] -= 1
+#         #     speLibre = np.delete(speLibre, np.where(speLibre == spe))
+#         #     etuLibre = np.delete(etuLibre, 0)
+#         # else:
+#         #     for i in range(len(tabSpe[spe])):
+#         #         if (tabSpe[spe][i] == etuLibre[0]):
+#         #             speLibre = np.append(speLibre, spe)
+#         #             etuLibre = np.delete(etuLibre, 0)
+#         #             break
+        
+
+
+def galeShapley(tabEtu, tabSpe, cap):
+    
+    #   Initialisation
+    etu_libres = list(range(len(tabEtu)))
+    capSpe = cap.copy() #list[int]    
+    dictEtu = {i: tabEtu[i] for i in range(len(tabEtu))} #dict etu - clé: num étu, bucket: liste pref etu (int: int)
+    dictSpe = {i: tabSpe[i] for i in range(len(tabSpe))} #dict spe - clé: num spé, bucket: liste pref spé
+    affectations = {} #dico résultats, clé: num spé, bucket: liste des étu choisis dans la spé (int, int)
+    
+    while (len(etu_libres)!=0): #tant qu'il reste un etu libre
+        num_i = etu_libres.pop()
+        prefs_etu_i = dictEtu[num_i]
+        spe_h = prefs_etu_i[0] #premier élément dans les prefs de i (retiré)
+        prefs_etu_i = np.delete(prefs_etu_i, 0)
+        
+        if (capSpe[spe_h] > 0): # H n'a pas atteint sa cap max
+            capSpe[spe_h] -= 1 #on diminue la capacité restante de H
+            
+            if spe_h not in affectations.keys(): 
+                affectations[spe_h] = []
+                
+            (affectations[spe_h]).append(num_i)
+
+
+        else: #on a atteint la capacité max de la spe_H
+            
+            max_pref = -1 #(index du moins pref de H)
+            worst_etu = None
+            for etu in affectations[spe_h]:
+                idx_pref = dictSpe[spe_h].index(etu) #indice de l'etu en cours dans les prefs de H
+                if (idx_pref > max_pref): #moins pref que celui d'avant
+                    max_pref = idx_pref
+                    worst_etu = etu
+            least_pref_H = worst_etu #dernier etu affecté dans H, le moins préféré de H
+            
+            if (dictSpe[spe_h].index(num_i)) < (dictSpe[spe_h].index(least_pref_H)): #h pref I à least_pref
+                affectations[spe_h].remove(least_pref_H) #supp du pire
+                etu_libres.append(least_pref_H) #rajout du pire dans les libres
+                affectations[spe_h].append(num_i) #ajout du meilleur (i)
+            
+            else: #H rejette la proposition de i
+                etu_libres.append(num_i)
+                
+        print(affectations)
+        return affectations
--- Ressources/main.py
+++ Ressources/main.py
@@ -1,17 +1,22 @@
 import tools
 import galeshapley as gs
 
-maListe=tools.lectureEtu("PrefEtu.txt")
-print(maListe)
+listeEtu=tools.lectureEtu("PrefEtu.txt")
 
-print(len(maListe)) #Longueur de la liste.
+# print(maListe)
 
-maList=tools.lectureSpe("PrefSpe.txt")
-print(maList[0])
-print(len(maList[0])) 
+# print(len(maListe)) #Longueur de la liste.
+
+specap = tools.lectureSpe("PrefSpe.txt")
+listeSpe = specap[0]
+capSpe = specap[1]
+
+# print(maList[0])
+# print(len(maList[0])) 
 
 
-print(type((maList[0])[0][0])) #Type de l'element d'indice 0 de la liste d'indice 0
+# print(type((maList[0])[0][0])) #Type de l'element d'indice 0 de la liste d'indice 0
 # exemple.createFichierLP(maListe[0][0],int(maListe[1][0])) #Methode int(): transforme la chaine de caracteres en entier
 
 # gs.galeShapley(maListe,maList)
+print(gs.galeShapley(listeEtu, listeSpe, capSpe))
